11c590372fabe73b0897a3749b303dd3dba9c7a7
VinnieKura <vinnie.kura@gmail.com>
2026-05-14 21:33:26 +1000

    Remove CSS import

--- src/pages/SubjectHub.jsx
+++ src/pages/SubjectHub.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useAuth } from '@clerk/clerk-react'
-import '../styles/SubjectHub.css'
 import { useNavigate } from 'react-router-dom'
 
 const SubjectHub = () => {
